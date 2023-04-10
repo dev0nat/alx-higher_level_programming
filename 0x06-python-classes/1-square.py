600d2717d53895dcf446265b4146f0a09799352e
dev0nat <natdev01@tuta.io>
2023-04-10 11:30:04 -0700

    readme added

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Define a new class Square"""
+
+
+class Square:
+    """class Square that defines a square"""
+    def __init__(self, size=0):
+        """Initialize a new Square"""
+        self.__size = size
